0375f01c452eb347991bc3d3a3dd6c6bec3d11e5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-04 15:31:17 +0100

    dev: added implementation of ellipsis

--- front/src/app/widget/ellipsis.pipe.ts
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -6,6 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class EllipsisPipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
+    if (typeof value !== 'string') {
+      throw new Error('pas bien...')
+    }
+    if (value.length > 10) {
+      return value.substring(0, 10) + '...'
+    }
     return value;
   }
 
