e04766e24b33c8d4f388eb7c929d2c4bec5f1585
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-04 15:47:33 +0100

    dev: added type on string

--- front/src/app/widget/ellipsis.pipe.ts
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -5,10 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsisPipe implements PipeTransform {
 
-  transform(value: unknown, maxLength = 10): unknown {
-    if (typeof value !== 'string') {
-      throw new Error('pas bien...')
-    }
+  transform(value: string, maxLength = 10): unknown {
     if (value.length > maxLength) {
       return value.substring(0, maxLength) + '...'
     }
