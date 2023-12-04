94a830affb0f92bdd5957f0ee1b2fcc1d8426958
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-04 15:37:08 +0100

    dev: added optrion to pipe

--- front/src/app/widget/ellipsis.pipe.ts
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -5,12 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsisPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: unknown, maxLength = 10): unknown {
     if (typeof value !== 'string') {
       throw new Error('pas bien...')
     }
-    if (value.length > 10) {
-      return value.substring(0, 10) + '...'
+    if (value.length > maxLength) {
+      return value.substring(0, maxLength) + '...'
     }
     return value;
   }
