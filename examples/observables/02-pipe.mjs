7c4f0c10be9926522c06794554ecb974e47a4f0c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-05 09:30:51 +0100

    dev: added obs tap

--- examples/observables/02-pipe.mjs
+++ examples/observables/02-pipe.mjs
@@ -1,23 +1,14 @@
-import { interval, startWith, map, take } from "rxjs";
+import { interval, startWith, map, take, tap } from "rxjs";
 
 // const obs = take(4)(map((x) => x + 1)(startWith(-1)(interval(1000))));
 
 const obs = interval(1000).pipe(
   startWith(-1),
   map((x) => x + 1),
-  take(4)
+  take(4),
+  tap((x) => {
+    console.log("x: ", x);
+  })
 );
 
-const observer = {
-  next: (x) => {
-    console.log("x: ", x);
-  },
-  error: (err) => {
-    console.log("err: ", err);
-  },
-  complete: () => {
-    console.log("complete");
-  },
-};
-
-obs.subscribe(observer);
+obs.subscribe();
