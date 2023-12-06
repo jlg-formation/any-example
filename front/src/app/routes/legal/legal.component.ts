56c8e5842583f38c58e85ee63f5f6dd147fd79d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-06 16:41:32 +0100

    dev: localstorage

--- front/src/app/routes/legal/legal.component.ts
+++ front/src/app/routes/legal/legal.component.ts
@@ -7,9 +7,19 @@ import { IHaveReadTheLegalPage } from '../../guards/legal.guard';
   styleUrls: ['./legal.component.scss'],
 })
 export class LegalComponent implements OnInit {
+  counter = 0;
   constructor() {}
 
   ngOnInit(): void {
     IHaveReadTheLegalPage();
+
+    const str = localStorage.getItem('toto');
+    if (str === null) {
+      localStorage.setItem('toto', '0');
+      return;
+    }
+    this.counter = Number(str);
+    this.counter++;
+    localStorage.setItem('toto', this.counter + '');
   }
 }
