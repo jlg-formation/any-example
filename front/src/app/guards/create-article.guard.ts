4473995516b448fa0606c1078f061033b3f4492f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-15 13:22:27 +0200

    fix: update createArticleGuard to redirect to legal page when article limit is exceeded

--- front/src/app/guards/create-article.guard.ts
+++ front/src/app/guards/create-article.guard.ts
@@ -1,11 +1,12 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { ArticleService } from '../services/article.service';
 
 export const createArticleGuard: CanActivateFn = (route, state) => {
   const articleService = inject(ArticleService);
+  const router = inject(Router);
   if (articleService.articles && articleService.articles?.length > 5) {
-    return false;
+    return router.parseUrl('/legal');
   }
   return true;
 };
