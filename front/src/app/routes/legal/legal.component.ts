87a7a3d262c81b51b9b415d2510d777f4f944f97
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-15 12:12:10 +0200

    feat: enhance legal component with real-time date display and cleanup logic

--- front/src/app/routes/legal/legal.component.ts
+++ front/src/app/routes/legal/legal.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+
+import { Subscription, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-legal',
   templateUrl: './legal.component.html',
-  styleUrls: ['./legal.component.scss']
+  styleUrls: ['./legal.component.scss'],
+  imports: [CommonModule],
 })
-export class LegalComponent implements OnInit {
+export class LegalComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  time = signal(new Date());
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.subscription = interval(1000)
+      .pipe(
+        tap(() => {
+          console.log('adjusting time');
+          this.time.set(new Date());
+        }),
+      )
+      .subscribe();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {}
 }
