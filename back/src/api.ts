f948263c378746a44c300ce6114f61b6ad411e1f
Jean-Louis GUENEGO (Orsys) <jlguenego@gmail.com>
2025-06-16 17:30:35 +0200

    feat: cors

--- back/src/api.ts
+++ back/src/api.ts
@@ -11,12 +11,12 @@ let articles: Article[] = [
 
 const app = Router();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "*");
+//   next();
+// });
 
 app.get("/articles", (req, res) => {
   res.json(articles);
