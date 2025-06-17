3bf785c39ead9574523eb4a110cfcf5da172ec29
Jean-Louis GUENEGO (Orsys) <jlguenego@gmail.com>
2025-06-17 16:47:50 +0200

    feat: signaux

--- front/src/app/routes/legal/legal.component.ts
+++ front/src/app/routes/legal/legal.component.ts
@@ -1,5 +1,13 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  linkedSignal,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-legal',
@@ -8,8 +16,34 @@ import { Component, OnInit } from '@angular/core';
   imports: [DatePipe],
 })
 export class LegalComponent implements OnInit {
-  now = new Date();
-  constructor() {}
+  now = signal(new Date());
 
-  ngOnInit(): void {}
+  readonly start = new Date();
+
+  chrono = linkedSignal(() => {
+    return ((this.now().getTime() - this.start.getTime()) / 1000).toFixed(0);
+  });
+
+  second = computed(() => {
+    if (Number(this.chrono()) < 2) {
+      return 'seconde';
+    }
+    return 'secondes';
+  });
+
+  constructor() {
+    interval(1000)
+      .pipe(
+        tap(() => {
+          this.now.set(new Date());
+          console.log('nouvelle val: ', this.now());
+        }),
+        takeUntilDestroyed(),
+      )
+      .subscribe();
+  }
+
+  ngOnInit(): void {
+    // this.chrono.set('-6');
+  }
 }
