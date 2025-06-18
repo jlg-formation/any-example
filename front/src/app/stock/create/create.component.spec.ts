e2d92ec4ea9d57c4a036ab4df279e50230a5cc9a
Jean-Louis GUENEGO (Orsys) <jlguenego@gmail.com>
2025-06-18 14:42:46 +0200

    test: compile

--- front/src/app/stock/create/create.component.spec.ts
+++ front/src/app/stock/create/create.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreateComponent } from './create.component';
+import CreateComponent from './create.component';
 
 describe('CreateComponent', () => {
   let component: CreateComponent;
@@ -8,9 +8,8 @@ describe('CreateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
